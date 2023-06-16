9cb2be6421c32cc4a705f868835aa43c411f0759
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-06-16 18:55:26 -0500

    path to notes.html and back

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
+const path = require('path');
+
+//path to notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+//path back 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
