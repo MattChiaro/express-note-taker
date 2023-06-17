3c239133bcc3329f88c2371db29cb792e25f7ad4
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-06-16 20:52:39 -0500

    housekeeping

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
+
 const express = require('express');
+const fs = require('fs');
+const { nanoid } = require('nanoid'); //random id gen
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
-const fs = require('fs');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
-
-
 
 //path to notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
-
+//grab data from db.json and display
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
         res.json(JSON.parse(data));
     });
 });
+
+
+
+
+
 
 //wildcard route to index.html
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
+
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
