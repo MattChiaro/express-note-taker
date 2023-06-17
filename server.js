b93472f3a1bf7b8ef9831df552be00af5a0857ff
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-06-16 19:30:36 -0500

    'get' route functioning, restructured server.js

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+const fs = require('fs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
-const path = require('path');
+
+
 
 //path to notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
-//path back 
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
+//wildcard route to index.html
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
